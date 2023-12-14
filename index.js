98d4b427f12c5f149f8cd37a8471da8f2c7ec2e1
davebrehm <148092344+davebrehm@users.noreply.github.com>
2023-12-14 14:38:49 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+
+var app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.listen(3000, () => {
+  console.log("Server is running on port: 3000");
+});
